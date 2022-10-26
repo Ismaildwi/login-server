df15cc0959866910bde41aa25329a3bc0b9542b9
Ismaildwin <101150684+Ismaildwi@users.noreply.github.com>
2022-10-26 22:51:27 +0700

    perbaruan

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {BrowserRouter, Route, Routes}from 'react-router-dom'
+import Profile from './pages/profile';
+
+function App() {
+  return (
+<>
+<BrowserRouter>
+<Routes>
+  <Route
+  path='/'
+  element={
+    <div className="App">
+      <a href="http://localhost:8000/api/v1/auth/google">Login pakek Google gaweane pak ardis</a>
+    </div>
+  }
+  />
+  <Route
+  path='/:token'
+  element = {
+    <Profile/>
+  } 
+  />
+</Routes>
+</BrowserRouter>
+</>
+ 
+  );
+}
+
+export default App;
---
+++ src/pages/profile.jsx
@@ -0,0 +1,27 @@
+import{useParams} from "react-router-dom"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
+const Profile = ()  => {
+    const{token} = useParams ()
+    const [data,setData] = useState()
+    const getData = async () => {
+        const {data} = await axios("http://localhost:8000/api/v1/profile", {
+            headers:{
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        setData(data);
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+    return (
+        <h1>Selamat Datang, {data?.data.email}</h1>
+    )
+}
+
+export default Profile
